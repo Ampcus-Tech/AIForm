581d86982ec79521b06b831ca0d8ff4d1fb0cbdd
Loknath prajapati <loknath.prajapati@ampcustech.com>
2026-01-13 00:21:59 +0530

    updated url

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+const API_BASE_URL = "https://sbeamp.ampcustech.info/api"||import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 
 console.log('🔗 API Base URL:', API_BASE_URL)
 
